88916e7377f463c9a44481e16fad3797d2357dfe
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-22 23:55:05 +0600

    created a class for component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
