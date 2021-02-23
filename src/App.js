427337b81804d81ece47e68285fe1956dea9354d
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-23 16:44:12 +0600

    completed array object

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ComOne from './component';
 
 function App() {
-  const names = ['joney', 'jerry', 'jubayer', 'roney'];
-
+  const products = [
+    { name: 'first', price: '$12' },
+    { name: 'second', price: '$15' },
+  ];
   return (
     <div>
-      <ComOne name={names[0]} bio={names[1]} />
-      <ComOne name={names[3]} />
-      <ComOne name="abul" bio="toto" />
-      <ComOne name="kabul" bio="bito" />
+      <ComOne name={products[0].name} price={products[0].price} />
+      <ComOne />
     </div>
   );
 }
--- src/component.jsx
+++ src/component.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 function ComOne(props) {
+  const priceStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: '#ddd',
+    height: '200px',
+    widht: '200px',
+    margin: '50px',
+  };
   return (
-    <div style={{ border: '2px solid #ddd', textAlign: 'center' }}>
-      <h1>{props.name}</h1>
-      <h2>{props.bio}</h2>
+    <div style={priceStyle}>
+      <h2>{props.name}</h2>
+      <h4>{props.price}</h4>
+      <button>Buy Now</button>
     </div>
   );
 }
