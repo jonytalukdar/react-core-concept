7eecd950b8493058a81cd1012444ed4928fc52ec
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-28 15:35:33 +0600

    added some style

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,11 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-import Product from './product';
-import Count from './count';
-import Api from './api';
 
-function App() {
-  const friendsList = ['shimul', 'akash', 'robin', 'rashudul', 'polash'];
-  const products = [
-    { name: 'photoshop', price: '$21' },
-    { name: 'XD', price: '$34' },
-    { name: 'Illustrator', price: '$56' },
-  ];
+const App = () => {
   return (
     <div>
-      <Api />
-
-      <Count />
-      <ul>
-        {friendsList.map((list) => (
-          <li>{list}</li>
-        ))}
-      </ul>
-      {products.map((pd) => (
-        <Product product={pd}></Product>
-      ))}
+      <h1>hello</h1>
     </div>
   );
-}
+};
 
 export default App;
