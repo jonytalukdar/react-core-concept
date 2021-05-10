5a0a65497821f03f1872b030819aa562e6a141f8
jonytalukdar <jonytalukdar30@gmail.com>
2021-05-10 23:14:57 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.count = 12;
+  //   this.state = {
+  //     count: 0,
+  //   };
+  // }
+  state = {
+    count: 0,
+  };
+
   render() {
+    const handle = () => {
+      this.setState({ count: this.state.count + 1 });
+    };
     return (
       <div>
         <h1>This is state</h1>
+        <h1>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            handle();
+            console.log('click', this.state.count);
+          }}
+        >
+          Increment
+        </button>
       </div>
     );
   }
