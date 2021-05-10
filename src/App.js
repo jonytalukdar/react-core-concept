e44f6cb2cebcde470cf4d3b3149316e03114e811
jonytalukdar <jonytalukdar30@gmail.com>
2021-05-10 16:14:18 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Count from './components/count/Count';
-import User from './components/user/User';
-import Fake from './fakeData/fake.json';
+import React, { Component } from 'react';
 
-const App = () => {
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    setUser(Fake);
-  });
-
-  const [count, setCount] = useState([]);
-  const handle = (userDetails) => {
-    const newCount = [...count, userDetails];
-    setCount(newCount);
-  };
-
-  return (
-    <div>
-      <h3>count : {count.length}</h3>
-      <Count cart={count}></Count>
-      <h1 style={{ textAlign: 'center' }}>user length : {user.length}</h1>
-      {user.map((list) => (
-        <User userDetails={list} handle={handle} key={list.id}></User>
-      ))}
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello react is awesome to learn</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
